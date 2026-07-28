9adc5eca9179c4bdc9528b422848891a118cb9e6
Peter Steinberger <steipete@gmail.com>
2026-07-28 03:17:44 -0700

    chore: reconcile ownership rebase artifacts

--- src/gateway/session-utils.test.ts
+++ src/gateway/session-utils.test.ts
@@ -26,8 +26,8 @@ import type { GatewayModelCatalogSnapshot } from "./server-model-catalog.types.j
 import { registerSessionAutomationSource } from "./session-automation-index.js";
 import { buildGatewaySessionEventFields } from "./session-event-payload.js";
 import { capArrayByJsonBytes } from "./session-transcript-readers.js";
-import { buildSingleRowStoreChildSessionsByKey } from "./session-utils-projection.js";
 import { listSessionsFromStore } from "./session-utils-list.js";
+import { buildSingleRowStoreChildSessionsByKey } from "./session-utils-projection.js";
 import {
   canonicalizeSpawnedByForAgent,
   buildGatewaySessionRow,
